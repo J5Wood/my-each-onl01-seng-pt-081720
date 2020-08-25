acf9ba893ed06070472e84b916671da061d53f76
Jeremy Wood <js.wood206@gmail.com>
2020-08-25 00:02:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,5 +2,11 @@ def my_each(array)# put argument(s) here
   # code here
   if array.length == 0
     puts "Received empty array"
-  while
+  else 
+    counter = 0
+    while counter < array.length
+      counter +=1
+      
+  end
+  array
 end
